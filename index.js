9d7a1a489e396b7e6060c6281aa9fc5e2f11ec13
Paul <Paul@mbp-van-paul.home>
2018-06-01 17:49:48 +0200

    no message

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ restService.use(bodyParser.json());
 
 
 
-exports.weatherWebhook = (req, res) => {
+// exports.weatherWebhook = (req, res) => {
+  restService.post("/echo", function(req, res) {
   // Get the city and date from the request
   let city = req.body.queryResult.parameters['geo-city']; // city is a required param
 

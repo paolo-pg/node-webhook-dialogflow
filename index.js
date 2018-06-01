71002e0ec932b363716474ca9cf900c0aba8ed3a
Paul <Paul@MacBook-Pro-van-Paul.local>
2018-06-01 15:14:57 +0200

    no message

--- index.js
+++ index.js
@@ -16,11 +16,28 @@ restService.use(bodyParser.json());
 restService.post("/echo", function(req, res) {
   var city = req.body.queryResult.parameters['geo-city'];
   var name = req.body.queryResult.parameters['name'];
-  var antwoord = "Allright " + name + ", the Cappucino is free today in " + city; 
+  // var antwoord = "Allright " + name + ", the Cappucino is free today in " + city;
+
+  https.get('http://www.omdbapi.com/?i=tt3896198&apikey=e65e58b8', (resp) => {
+  let data = '';
+ 
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+ 
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).Title);
+  });
+ 
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+}); 
   
 
   return res.json({
-    fulfillmentText: antwoord
+    fulfillmentText: JSON.parse(data).Title
   });
 });
 

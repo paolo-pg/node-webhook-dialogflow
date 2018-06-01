b854109b583e2fd7ba8ff182ed0cf2cf5c6ff791
Paul <Paul@mbp-van-paul.home>
2018-06-01 18:50:29 +0200

    no message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const express = require("express");
-const bodyParser = require("body-parser");
+// const express = require("express");
+// const bodyParser = require("body-parser");
 
 const http = require('http');
 

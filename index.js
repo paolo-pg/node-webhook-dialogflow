a346e82c4e8f97c7a909316f0908244186317d8e
Paul <Paul@mbp-van-paul.home>
2018-06-04 19:21:48 +0200

    no message

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
-  // var city = req.body.queryResult.parameters['geo-city'];
-  // var name = req.body.queryResult.parameters['name'];
+  var city = req.body.queryResult.parameters['geo-city'];
+  var name = req.body.queryResult.parameters['name'];
   // var antwoord = "Allright " + name + ", the Cappucino is free today in " + city;
 
   https.get('https://api.worldweatheronline.com/premium/v1/weather.ashx?key=86840a75efc34f51aaa130030182905&q=Eindhoven&format=json&date=today', (resp) => {
@@ -30,7 +30,7 @@ restService.post("/echo", function(req, res) {
   resp.on('end', () => {
     fulfillmentText: JSON.parse(data).data.request[0].query
      return res.json({
-    fulfillmentText: JSON.parse(data).data.current_condition[0].temp_C
+    fulfillmentText: JSON.parse(data).data.current_condition[0].weatherDesc[0].value
   });
   });
  

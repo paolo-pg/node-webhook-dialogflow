e443ceb9662923a9aa3ee852a4363ecd11241032
Paul <Paul@mbp-van-paul.home>
2018-06-05 18:23:36 +0200

    no message

--- index.js
+++ index.js
@@ -14,56 +14,23 @@ restService.use(
   bodyParser.urlencoded({
     extended: true
   })
-  );
+);
 
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
 
   console.log(req.body.queryResult.intent.displayName);
-   // try {
-   //      if (req.body && req.body.queryResult) {
-   //          var body = req.body;
-
-   //          if (body.queryResult.fulfillment) {
-   //              console.log(body.queryResult.fulfillmentText);
-   //          }
-
-   //          if (body.queryResult.action && currentAction.action != body.queryResult.action) {
-   //              console.log("Updating action to: " + body.queryResult.action);
-   //              currentAction.action = body.queryResult.action;
-   //              res.sendStatus(200);
-   //          } else {
-   //              return res.status(400).json({
-   //                  status: {
-   //                      code: 400,
-   //                      failedAction: body.queryResult.action
-   //                  }
-   //              });
-   //          }
-   //      }
-
-   //  } catch (err) {
-   //      console.error("Can't process request", err);
-
-   //      return res.status(400).json({
-   //          status: {
-   //              code: 400,
-   //              errorType: err.message
-   //          }
-   //      });
-   //  }
-
-
-
-
+  
    var city = req.body.queryResult.parameters['geo-city'];
    var name = req.body.queryResult.parameters['name'];
-  // var antwoord = "Allright " + name + ", the Cappucino is free today in " + city;
+
+   var drinks = ['Americano', 'Espresso', 'Cappuccino', 'Cafe Latte', 'Flat White'];
+   var rand = drinks[Math.floor(Math.random() * drinks.length)];
 
   if (req.body.queryResult.intent.displayName == "user.name.location") {
-        https.get('https://api.worldweatheronline.com/premium/v1/weather.ashx?key=86840a75efc34f51aaa130030182905&q=Eindhoven&format=json&date=today', (resp) => {
-          let data = '';
+    https.get('https://api.worldweatheronline.com/premium/v1/weather.ashx?key=86840a75efc34f51aaa130030182905&q=Eindhoven&format=json&date=today', (resp) => {
+      let data = '';
 
       // A chunk of data has been recieved.
       resp.on('data', (chunk) => {
@@ -83,13 +50,14 @@ restService.post("/echo", function(req, res) {
     }).on("error", (err) => {
       console.log("Error: " + err.message);
     }); 
-}
-if (req.body.queryResult.intent.displayName == "user.name.location - yes") {
-  return res.json({
+  }
+
+  if (req.body.queryResult.intent.displayName == "user.name.location - yes") {
+    return res.json({
           // fulfillmentText: "Allright " + name + ". I would the describe the weather as " + weatherDescription + " in " + city + " today. Would you like to order something?" 
-          fulfillmentText: "YOU JUST SAID YES" 
+          fulfillmentText: "Great! The special offer in  " + city + " today is: a free " + rand + " ! Would you like to have one?"
         });
-};
+  };
 
 
 

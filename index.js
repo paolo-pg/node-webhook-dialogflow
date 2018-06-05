d369e5ade641a75bbdfb10f0ea4f501f875dda0a
Paul <Paul@mbp-van-paul.home>
2018-06-05 18:00:14 +0200

    no message

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ restService.use(
   bodyParser.urlencoded({
     extended: true
   })
-);
+  );
 
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
-  console.log("Hieronder try");
+
   console.log(req.body.queryResult.intent.displayName);
    // try {
    //      if (req.body && req.body.queryResult) {
@@ -57,34 +57,44 @@ restService.post("/echo", function(req, res) {
 
 
 
-  var city = req.body.queryResult.parameters['geo-city'];
-  var name = req.body.queryResult.parameters['name'];
+   var city = req.body.queryResult.parameters['geo-city'];
+   var name = req.body.queryResult.parameters['name'];
   // var antwoord = "Allright " + name + ", the Cappucino is free today in " + city;
 
-  https.get('https://api.worldweatheronline.com/premium/v1/weather.ashx?key=86840a75efc34f51aaa130030182905&q=Eindhoven&format=json&date=today', (resp) => {
-  let data = '';
- 
-  // A chunk of data has been recieved.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
- 
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    var weatherOutput = JSON.parse(data).data.current_condition[0].weatherDesc[0].value
-    var weatherDescription  = weatherOutput.toLowerCase();
-      return res.json({
-      // fulfillmentText: "Allright " + name + ". I would the describe the weather as " + weatherDescription + " in " + city + " today. Would you like to order something?" 
-      fulfillmentText: "Allright " + name + ". because the weather is " + weatherDescription + " in " + city + " today, i've got a special offer for you. Do you want to know what special offer i have for you?" 
-    });
-  });
- 
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-}); 
-  
+  if (req.body.queryResult.intent.displayName == "user.name.location") {
+        https.get('https://api.worldweatheronline.com/premium/v1/weather.ashx?key=86840a75efc34f51aaa130030182905&q=Eindhoven&format=json&date=today', (resp) => {
+          let data = '';
 
- 
+      // A chunk of data has been recieved.
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        var weatherOutput = JSON.parse(data).data.current_condition[0].weatherDesc[0].value
+        var weatherDescription  = weatherOutput.toLowerCase();
+        return res.json({
+          // fulfillmentText: "Allright " + name + ". I would the describe the weather as " + weatherDescription + " in " + city + " today. Would you like to order something?" 
+          fulfillmentText: "Allright " + name + ". because the weather is " + weatherDescription + " in " + city + " today, i've got a special offer for you. Do you want to know what special offer i have for you?" 
+        });
+      });
+
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+    }); 
+}
+if (req.body.queryResult.intent.displayName == "user.name.location - yes") {
+  return res.json({
+          // fulfillmentText: "Allright " + name + ". I would the describe the weather as " + weatherDescription + " in " + city + " today. Would you like to order something?" 
+          fulfillmentText: "YOU JUST SAID YES" 
+        });
+};
+
+
+
+
+
 });
 
 
